28f8a5c4b6a7e1fe09aea47210282e069898a338
2006ShaguftaAdhoni <shaguftaadhoni@gmail.com>
2026-09-05 16:56:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Giftify is ready! 🎁");
+let cart = [];
+
+const cartButtons = document.querySelectorAll("article button");
+
+cartButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const product = button.parentElement.querySelector("h3").textContent;
+
+        cart.push(product);
+
+        alert(product + " added to cart! 🛒");
+    });
+});
