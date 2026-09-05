8e01495550cecb15fb2c75024375deaa812309ef
2006ShaguftaAdhoni <shaguftaadhoni@gmail.com>
2026-09-05 16:59:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ cartButtons.forEach((button) => {
         alert(product + " added to cart! 🛒");
     });
 });
+function showCart() {
+    const cartDisplay = document.getElementById("cartDisplay");
+
+    if (cart.length === 0) {
+        cartDisplay.innerHTML = "<h2>Your cart is empty 🛒</h2>";
+        return;
+    }
+
+    cartDisplay.innerHTML = "<h2>Your Cart 🛒</h2>";
+
+    cart.forEach((product) => {
+        cartDisplay.innerHTML += "<p>🎁 " + product + "</p>";
+    });
+}
