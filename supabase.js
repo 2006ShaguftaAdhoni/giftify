7540aac390ee8986b39ab592df1d8f5656546d56
2006ShaguftaAdhoni <shaguftaadhoni@gmail.com>
2026-09-07 16:43:26 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://zxmqkymrsyobqkqdqdtf.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_UT18tmWiUIMiPTa4YGtAaQ_AWDKY26m";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
